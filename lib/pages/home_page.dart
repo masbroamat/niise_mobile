d1142b7fbc45304237399a4f991cb08256072783
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2025-03-17 12:57:36 +0800

    remove unusedimport in home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:niise_mobile/pages/settings_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
